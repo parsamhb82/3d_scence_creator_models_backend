9c6c87567a9b211c2a62e11467735e1ed8dc0987
parsamhb82 <parsa1382mohebian@gmail.com>
2025-08-11 17:28:33 +0330

    chaged the urls

--- scene_creator_apis/scene_creator_apis/urls.py
+++ scene_creator_apis/scene_creator_apis/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('mesh_detector.urls')),
+    path('ai/', include('mesh_detector.urls')),
 ]
